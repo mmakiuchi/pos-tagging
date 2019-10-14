c0b1a70b71bf81b1da2b4855272a6b447f9b4088
Mariana Makiuchi <marimakiuchi@gmail.com>
2019-10-14 11:39:59 +0900

    Fixed the cross-validation definition

--- pos_baseline.py
+++ pos_baseline.py
@@ -16,9 +16,8 @@ import pos_helpers as helper
 import random
 
 # file names
-train_file_name = "train.txt"
-test_file_name = "test.txt"
-freq_pos_file_name = "POStags.txt" # output file with most frequent POS
+file_name = "train.txt"
+# freq_pos_file_name = "POStags.txt" # output file with most frequent POS
 
 # applies the baseline POS tagging
 def pos_tagging(word_list, tag_dict):
@@ -53,80 +52,56 @@ def compare_gold_standard(pos_tag, gold_standard):
     return(correct/(correct + wrong))
 
 
-# performs the cross validation on a given partition
-def cross_validation(word_partition, pos_dict, gold_partition):
+if __name__ == "__main__":
+    # k-fold cross validation
+    k = 10
 
-    # send the word partition to be POS tagged
-    pos_tags = pos_tagging(word_partition, pos_dict)
+    # read the file
+    line_list = helper.read_lines(file_name)
+    num_lines = len(line_list)
 
-    # compare the prediction to the gold standard
-    accuracy = compare_gold_standard(pos_tags, gold_partition)
-
-    return(accuracy)
-
-# generates 10 random partitions and perform the cross validation
-def ten_fold_cross_val(word_list, pos_dict, gold_standard):
-    
-    cross_val_acc = 0 
     random.seed()
-    max_size = len(word_list)
-    print('Number of words is:', max_size)
-    print('Each partition will have size: ', max_size // 10)
+    cross_val_acc = 0
 
-    for i in range(10):
+    for i in range(k):
         print('Trial ', i+1)
-        part_start = random.randint(0, (max_size - (max_size // 10)) )
 
-        # get a random partition of the word list of size max_size // 10
-        word_partition = word_list[part_start:part_start+(max_size // 10)]
+        # the test partition will be 20% of the total file
+        test_start = random.randint(0, (num_lines - (num_lines // 5)) )
+        test_end = test_start + num_lines // 5
 
-        # get the corresponding gold_standard for that partition
-        gold_partition = gold_standard[part_start:part_start+(max_size // 10)]
+        # separate train and test partitions
+        train_part = line_list[0:test_start] + line_list[test_end:]
+        test_part = line_list[test_start:test_end]
 
-        # perform the cross-validation on the partition
-        accuracy = cross_validation(word_partition, pos_dict, gold_partition)
+        # transform the train lines
+        train_lines = helper.transf_lines(train_part)
+
+        # get frequency of each word/POS pair
+        freq_list = helper.get_frequency_list(train_lines)
+
+        # remove the duplicates from the train partition
+        train_list = list(dict.fromkeys(train_lines))
+
+        # create a list with the frequency and the lines
+        pos_freq = list(zip(train_list, freq_list))
+
+        # order the list in alphabetic order
+        pos_freq = sorted(pos_freq)
+
+        # get the most frequent tag for each word
+        pos_freq_list, pos_dict = helper.get_most_frequent_pos(pos_freq)
+
+        # prepare the data for the pos tagging
+        test_word_list, gold_standard = helper.prepare_data(test_part)
+
+        # get the POS tags after the tagging according to the baseline
+        pos_tags = pos_tagging(test_word_list, pos_dict)
+
+        # compare the prediction to the gold standard
+        accuracy = compare_gold_standard(pos_tags, gold_standard)
 
         cross_val_acc += accuracy
 
-    cross_val_acc = cross_val_acc/10
-
-    print('Ten-fold cross-validation accuracy: ', float("{0:.2f}".format(100*cross_val_acc)), '%')
-
-
-if __name__ == "__main__":
-    line_list = helper.read_lines(train_file_name)
-    transformed_lines = helper.transf_lines(line_list)
-
-    # get frequency of each word/POS pair
-    freq_list = helper.get_frequency_list(transformed_lines)
-
-    # remove the duplicates from the transformed lines
-    line_list = list(dict.fromkeys(transformed_lines))
-
-    # create a list with the frequency and the lines
-    pos_freq = list(zip(line_list, freq_list))
-
-    # order the list in alphabetic order
-    pos_freq = sorted(pos_freq)
-
-    # get the most frequent tag for each word
-    pos_freq_list, pos_dict = helper.get_most_frequent_pos(pos_freq)
-    
-    # save the most frequent POS tag for each word
-    helper.save_file(freq_pos_file_name, pos_freq_list)
-
-    # get the data to make the POS tagging
-    test_data = helper.read_lines(test_file_name)
-
-    # prepare the data for the pos tagging
-    print('Preparing data for tagging')
-    word_list, gold_standard = helper.prepare_data(test_data)
-
-    # get the POS tags after the tagging according to the baseline
-    print('Making the POS tagging')
-    tag_list = pos_tagging(word_list, pos_dict)
-
-    # compare predictions with gold standard
-    print('Ten fold cross-validation')
-    ten_fold_cross_val(word_list, pos_dict, gold_standard)
-    
+    cross_val_acc = cross_val_acc/k
+    print(k,'-fold cross-validation accuracy: ', float("{0:.2f}".format(100*cross_val_acc)), '%')
